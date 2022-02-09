fadf3f6ad2e3211e2e9cf1a6e6ecb1ba7d71a29d
= <=>
2022-02-09 22:09:02 +0100

    Upgraded Main.storyboard

--- Quizzet/ViewController.swift
+++ Quizzet/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        questionLabel.text = "Four + Two is Equal to Six"
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
