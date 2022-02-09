932a86d1da91c1f52ccc16675bc51c07455bc000
= <=>
2022-02-09 23:07:20 +0100

    Checking answer added

--- Quizzet/ViewController.swift
+++ Quizzet/ViewController.swift
@@ -12,13 +12,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
+    
+    var quiz = [
+        ["Four + Two is Equal to Six.", "True"],
+        ["Five - Three is greater than One", "True"],
+        ["Three + Eight is less than Ten", "False"]
+    ]
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        questionLabel.text = "Four + Two is Equal to Six"
+        UpdateUI()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        }else{
+            print("Wrong!")
+        }
+        
+        if questionNumber < 3 {
+            questionNumber += 1
+            UpdateUI()
+        }
+        
+    }
+    
+    func UpdateUI(){
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 }
