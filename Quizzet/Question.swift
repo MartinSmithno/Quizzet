c747beadca65538f140824598a6f3558724e5b5a
= <=>
2022-02-10 21:17:25 +0100

    Created Question structure

---
+++ Quizzet/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  Quizzet
+//
+//  Created by Martin Smith on 10/02/2022.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
--- Quizzet/ViewController.swift
+++ Quizzet/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     var quiz = [
-        ["Four + Two is Equal to Six.", "True"],
-        ["Five - Three is greater than One", "True"],
-        ["Three + Eight is less than Ten", "False"]
+        Question(text: "Four + Two is Equal to Six.", answer: "True"),
+        Question(text: "Five - Three is greater than One", answer: "True"),
+        Question(text: "Three + Eight is less than Ten", answer: "False")
     ]
+
+/*  Question(text: "Four + Two is Equal to Six.", answer: "True") */
+    
     var questionNumber = 0
     
     override func viewDidLoad() {
@@ -27,7 +30,8 @@ class ViewController: UIViewController {
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         let userAnswer = sender.currentTitle!
-        let actualAnswer = quiz[questionNumber][1]
+//      let actualQuestion = quiz[questionNumber]
+        let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
             print("Right!")
@@ -35,15 +39,16 @@ class ViewController: UIViewController {
             print("Wrong!")
         }
         
-        if questionNumber < 3 {
+        if questionNumber + 1 < quiz.count {
             questionNumber += 1
-            UpdateUI()
+        }else{
+            questionNumber = 0
         }
-        
+        UpdateUI()
     }
     
     func UpdateUI(){
-        questionLabel.text = quiz[questionNumber][0]
+        questionLabel.text = quiz[questionNumber].text
     }
     
 }
